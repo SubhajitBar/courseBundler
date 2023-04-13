954227256502715e196f2ffc4d0048099ef0ce94
SubhajitBar <pvt.subha@gmail.com>
2023-04-13 11:14:23 +0530

    payment router & payment controller modified

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { buySubscription, cancelSubscription, getRazorPayKey, paymentverification } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification,  } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.route("/subscribe").get( isAuthenticated, buySubscription);
 
 // Verify Payment and save reference in database
-router.route("/paymentverification").post(isAuthenticated, paymentverification);
+router.route("/paymentverification").post(isAuthenticated, paymentVerification );
 
 // Get Razorpay key
 router.route("/razorpaykey").get(getRazorPayKey );
